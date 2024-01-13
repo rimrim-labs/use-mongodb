5bad44a8ffa3e0f776eea35b90c5d7e5d86e8f53
rimrim990 <lim990718@naver.com>
2024-01-13 23:42:41 +0900

    fix: error 를 직접 던지지 않고 미들웨어로 넘기도록 수정

--- src/item/index.ts
+++ src/item/index.ts
@@ -3,21 +3,33 @@ import itemService from './service/itemService'
 
 const router = Router()
 
-router.post('/', async function (req, res) {
-  const { item } = req.body
-  const insertedId = await itemService.save(item)
-  return res.status(201).location(`/items/${insertedId}`).send(insertedId)
+router.post('/', async function (req, res, next) {
+  try {
+    const { item } = req.body
+    const insertedId = await itemService.save(item)
+    return res.status(201).location(`/items/${insertedId}`).send(insertedId)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/', async function (req, res) {
-  const items = await itemService.findAll()
-  return res.send(items)
+router.get('/', async function (req, res, next) {
+  try {
+    const items = await itemService.findAll()
+    return res.send(items)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', async function (req, res) {
-  const { id } = req.params
-  const item = await itemService.findOne(+id)
-  return res.send(item)
+router.get('/:id', async function (req, res, next) {
+  try {
+    const { id } = req.params
+    const item = await itemService.findOne(+id)
+    return res.send(item)
+  } catch (err) {
+    next(err)
+  }
 })
 
 export default router
