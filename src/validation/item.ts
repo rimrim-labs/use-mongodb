54de276568fb00ab94141266443538ecc8413453
rimrim990 <lim990718@naver.com>
2024-02-04 16:58:23 +0900

    refactor: locale 설정 분리

--- src/validation/item.ts
+++ src/validation/item.ts
@@ -1,23 +1,10 @@
-import { date, number, object, setLocale, string } from 'yup'
+import yup from './config'
 
-setLocale({
-  mixed: {
-    required: (params) => `${params.path} 은(는) 필수 값입니다.`,
-    notType: (params) => `${params.path} 은(는) ${params.type} 타입이어야 합니다.`,
-  },
-  number: {
-    integer: (params) =>
-      `${params.path} 은(는) 정수 값이어야 합니다. 주어진 값: ${params.originalValue}`,
-    positive: (params) =>
-      `${params.path} 은(는) 양수 값이어야 합니다. 주어진 값: ${params.originalValue}`,
-  },
-})
-
-export const CreateItemSchema = object({
-  id: number().integer().positive().required(),
-  name: string().required(),
-  remain: number().integer().positive().required(),
-  price: number().integer().positive().required(),
-  owner: string().required(),
-  createdAt: date().required(),
+export const CreateItemSchema = yup.object({
+  id: yup.number().integer().positive().required(),
+  name: yup.string().required(),
+  remain: yup.number().integer().positive().required(),
+  price: yup.number().integer().positive().required(),
+  owner: yup.string().required(),
+  createdAt: yup.date().required(),
 })
